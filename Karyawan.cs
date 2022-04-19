d3a63d55d4c159eaf6e4575d7e86bb8728314147
NandaGilang11 <101609523+NandaGilang11@users.noreply.github.com>
2022-04-19 12:12:38 +0700

    Add files via upload

---
+++ Karyawan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4355
+{
+    class Karyawan
+    {
+        public int NIK { get; set; }
+        public string Nama { get; set; }
+        public int Gaji { get; set; }
+        public int NaikGaji { get; set; }
+
+        public Karyawan(int nik, string nama, int gaji)
+        {
+            this.NIK = nik;
+            this.Nama = nama;
+
+            if (gaji < 0)
+            {
+                this.Gaji = 0;
+                NaikGaji = Convert.ToInt32((Gaji * 1.1));
+            }
+            else
+            {
+                this.Gaji = gaji;
+                NaikGaji = Convert.ToInt32((Gaji * 1.1));
+            }
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4355
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan(9865666, "Dominic", 5000000);
+            Karyawan karyawan2 = new Karyawan(2244352, "Hannams", 7000000);
+
+
+            
+            Console.WriteLine(" NO   NIK / NAMA\t\t   Gaji Bulanan\t  ");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine(" 1.  {0} {1} \t\t  {2}\t ", karyawan1.NIK, karyawan1.Nama, karyawan1.Gaji);
+            Console.WriteLine(" 2.  {0} {1} \t\t  {2}\t ", karyawan2.NIK, karyawan2.Nama, karyawan2.Gaji);
+
+            Console.WriteLine("\n\nAsikkkkk dpt kenaikan gaji nih!!!\n");
+
+            Console.WriteLine(" NO   NIK / NAMA\t\t   Gaji Bulanan\t  ");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine(" 1.  {0} {1} \t\t  {2}\t ", karyawan1.NIK, karyawan1.Nama, karyawan1.NaikGaji);
+            Console.WriteLine(" 2.  {0} {1} \t\t  {2}\t ", karyawan2.NIK, karyawan2.Nama, karyawan2.NaikGaji);
+
+
+            Console.ReadKey();
+        }
+    }
+}
